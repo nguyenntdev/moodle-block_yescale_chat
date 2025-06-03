675a48ee5ebee667a3c5d82b9815acfca2bb1f5a
Thien Nguyen <kakangocthien109@gmail.com>
2025-06-03 23:32:08 +0700

    YeScale API update
    Replaced endpoints and added new models in openai group.

--- lib.php
+++ lib.php
@@ -66,7 +66,7 @@ function fetch_assistants_array($block_id = null) {
         ),
     ));
 
-    $response = $curl->get("https://api.openai.com/v1/assistants?order=desc");
+    $response = $curl->get("https://api.yescale.io/v1/assistants?order=desc");
     $response = json_decode($response);
     $assistant_array = [];
     if (property_exists($response, 'data')) {
@@ -88,48 +88,25 @@ function get_models() {
     return [
         "models" => [
             'gpt-4o' => 'gpt-4o',
-            'gpt-4o-2024-11-20' => 'gpt-4o-2024-11-20',
-            'gpt-4o-2024-08-06' => 'gpt-4o-2024-08-06',
-            'gpt-4o-2024-05-13' => 'gpt-4o-2024-05-13',
-            'gpt-4o-mini-2024-07-18' => 'gpt-4o-mini-2024-07-18',
             'gpt-4o-mini' => 'gpt-4o-mini',
-            'gpt-4-turbo-preview' => 'gpt-4-turbo-preview',
-            'gpt-4-turbo-2024-04-09' => 'gpt-4-turbo-2024-04-09',
+            'gpt-4.1-2025-04-14' => 'gpt-4.1-2025-04-14',
+            'gpt-4.1-mini-2025-04-14' => 'gpt-4.1-mini-2025-04-14',
+            'gpt-4.1-nano-2025-04-14' => 'gpt-4.1-nano-2025-04-14',
             'gpt-4-turbo' => 'gpt-4-turbo',
-            'gpt-4-32k-0314' => 'gpt-4-32k-0314',
-            'gpt-4-1106-preview' => 'gpt-4-1106-preview',
-            'gpt-4-0613' => 'gpt-4-0613',
-            'gpt-4-0314' => 'gpt-4-0314',
-            'gpt-4-0125-preview' => 'gpt-4-0125-preview',
-            'gpt-4' => 'gpt-4',
-            'gpt-3.5-turbo-16k-0613' => 'gpt-3.5-turbo-16k-0613',
-            'gpt-3.5-turbo-16k' => 'gpt-3.5-turbo-16k',
-            'gpt-3.5-turbo-1106' => 'gpt-3.5-turbo-1106',
-            'gpt-3.5-turbo-0125' => 'gpt-3.5-turbo-0125',
-            'gpt-3.5-turbo' => 'gpt-3.5-turbo'
+            'gpt-3.5-turbo' => 'gpt-3.5-turbo',
+            'gpt-4' => 'gpt-4'
         ],
         "types" => [
-            'gpt-4o-2024-11-20'          =>  'chat',
-            'gpt-4o-2024-08-06'          =>  'chat',
-            'gpt-4o-2024-05-13'          =>  'chat',
-            'gpt-4o'                     =>  'chat',
-            'gpt-4o-mini-2024-07-18'     =>  'chat',
-            'gpt-4o-mini'                =>  'chat',
-            'gpt-4-turbo-preview'        =>  'chat',
-            'gpt-4-turbo-2024-04-09'     =>  'chat',
-            'gpt-4-turbo'                =>  'chat',
-            'gpt-4-32k-0314'             =>  'chat',
-            'gpt-4-1106-preview'         =>  'chat',
-            'gpt-4-0613'                 =>  'chat',
-            'gpt-4-0314'                 =>  'chat',
-            'gpt-4-0125-preview'         =>  'chat',
-            'gpt-4'                      =>  'chat',
-            'gpt-3.5-turbo-16k-0613'     =>  'chat',
-            'gpt-3.5-turbo-16k'          =>  'chat',
-            'gpt-3.5-turbo-1106'         =>  'chat',
-            'gpt-3.5-turbo-0125'         =>  'chat',
-            'gpt-3.5-turbo'              =>  'chat'
+            'gpt-4o'                     => 'chat',
+            'gpt-4o-mini'                => 'chat',
+            'gpt-4.1-2025-04-14'         => 'chat',
+            'gpt-4.1-mini-2025-04-14'    => 'chat',
+            'gpt-4.1-nano-2025-04-14'    => 'chat',
+            'gpt-4-turbo'                => 'chat',
+            'gpt-3.5-turbo'              => 'chat',
+            'gpt-4'                      => 'chat'
         ]
+
     ];
 }
 
